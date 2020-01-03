d8b759afbb3f95ca7d8b44d7091602ae6c6870dc
niveditha1yv <nivedita.y.v@gmail.com>
2020-01-03 23:27:48 +0530

    3 files modified, 1 added

---
+++ Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Form extends React.Component{
+  render(){
+    return(
+      <form action="">
+    	  <input type="text" placeholder="GitHub username"/>
+        <button>Add card</button>
+    	</form>
+    )
+  }
+}
+
+export default Form;
--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import Card from './Card';
+import Form from './Form';
 import './style.css';
 
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+const CardList = (props) => (
+  
+ <div> 
+ {testData.map(profile => <Card {...profile} />)}
+  </div>
+);
+
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      profiles:testData
+    }
+  }
   render(){
     return(
       <div>
         <div className="header">{this.props.title}</div> 
+        <Form />
+        <CardList profiles={this.state.profiles}/>
       </div>
     )
   }
